4f24ebf094c7f23c63286af4ce84286c12de2703
Bijon Miles <bijonmiles01@gmail.com>
2018-09-11 08:44:16 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
 def unsafe?(speed)
-  speed > 60 ? true : false
+  
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed < 40 or speed > 60
+	  puts " Going 30 mph on the freeway would be unsafe, as would going 95 mph"
 end
 	
 
